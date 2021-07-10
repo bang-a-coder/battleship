b9ba3f2308a66aaefaf1365c46ec4bd1a41e673a
bang-a-coder <nikitas4717@gmail.com>
2021-07-10 23:33:15 +0300

    ship hit works and tested

--- src/ship.js
+++ src/ship.js
@@ -5,7 +5,13 @@ class Ship {
 		this.sunk = obj.sunk
 
 		for (let i=0; i<obj.len; i++) { this.hull.push(0) }	
+	}
 
+	hit(n){
+		if (n > this.hull.length) return this.hull
+
+		this.hull[n] = 1
+		return this.hull
 	}
 
 
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -11,4 +11,29 @@ test('init ship', () => {
 		hull: [0,0,0],
 		sunk: false
 	})
-      })
+})
+
+let ship = _ship({
+	name: 'cruiser',
+	len: 3,
+	sunk: false
+})
+
+test('hit 2', ()=>{
+	expect(ship.hit(2)).toEqual([0,0,1])
+})
+
+test('hit 1', ()=>{
+	expect(ship.hit(1)).toEqual([0,1,1])
+})
+
+test('hit 0', ()=>{
+	expect(ship.hit(0)).toEqual([1,1,1])
+})
+
+test('hit 4', ()=>{
+	expect(ship.hit(4)).toEqual([1,1,1])
+})
+
+
+
