ffeb1eaf7b16646fc71dea68a070267867e4532e
bang-a-coder <nikitas4717@gmail.com>
2021-07-11 19:00:42 +0300

    placing ship upright left corner, no name tested and works

--- src/gameboard.js
+++ src/gameboard.js
@@ -3,21 +3,56 @@ class Gameboard{
 		this.grid = []
 
 		this.createGrid(n)
+		// 0 -> empty slot
+		// 1 -> boat there
+		// 2 -> hit miss
+		// 3 -> hit boat
 	}
 
 	createGrid(n){
 		for (let i = 0; i < n; i++){
 			this.grid[i] = []
-			for (let j = 0; j < n; j++){
-				this.grid[i][j] = 0
-			}
+			for (let j = 0; j < n; j++){ this.grid[i][j] = 0 }
 		}
 
 		return this.grid
 	}
 
+	placeShip(ship, orient, pin) {
+
+		let len = ship
+		let cors = decipherCors(pin)
+
+		if(orient === 'x'){
+			for (let i =0; i<len; i++){
+				this.grid[cors[0]][cors[1]+i] = 1
+			}
+		}
+
+		if(orient === 'y'){
+			for (let i =0; i<len; i++){
+				this.grid[cors[0]+i][cors[1]] = 1
+			}
+		}
+	
+
+		console.log(this.grid)
+		return this.grid
+	}
+
+
 }
 
 export function _gameboard(obj){
 	return new Gameboard(obj)
+}
+
+export function decipherCors(str){
+	let abRef = 'ABCDEFGHIJ'
+
+	let brokenStr = str.split('')
+
+	let arr = [abRef.indexOf(brokenStr[0]), parseInt(brokenStr[1])]
+
+	return arr
 }
