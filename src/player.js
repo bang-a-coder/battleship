bf6025c2747dfb291fcfff02b9ebf54aa334b1ab
bang-a-coder <nikitas4717@gmail.com>
2021-07-13 01:50:38 +0300

    player base stuff, not tested

---
+++ src/player.js
@@ -0,0 +1,36 @@
+import { _gameboard } from "./gameboard"
+
+class Player {
+	constructor(){
+		this.gameboard = _gameboard(10)
+		this.score = 0
+		this.shipOptions = ['carrier', 'battleship', 'cruiser', 'sub', 'destroyer']
+	}
+
+	positionVessels(orientation, pin){
+		this.gameboard.placeShip(this.shipOptions[0], orientation, pin)
+
+		return this.gameboard.grid
+	}
+
+	sendAttack(enemy, coordinates){
+		enemy.gameboard.receiveAttack(coordinates)
+	}
+
+	getEnemyMap(enemy){
+		let enemyBoordCopy = enemy.gameboard
+		enemyBoordCopy.grid.forEach((el)=>{
+			el.forEach((cor)=>{
+				if (cor === 1) {cor = 0}
+			})
+		})
+
+		//TODO refactor, send changes instead of iterating through arrays every time, lazy ass
+
+		return enemyBoordCopy.grid
+	}
+}
+
+export function _player(){
+	return new Player()
+}
